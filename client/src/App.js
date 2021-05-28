2764897d510a23b1dfe37fc3fcfe780f3945369a
jeffubayi <jeffubayi@gmail.com>
2021-05-28 13:34:13 +0300

    admin - add a menuItemIcon

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import HomeWorkIcon from '@material-ui/icons/HomeWork';
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import StarsIcon from '@material-ui/icons/Stars';
 import ChatIcon from '@material-ui/icons/Chat';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 //API FOR Posts and Users data from https://jsonplaceholder.typicode.com
 import jsonServerProvider from 'ra-data-json-server';
 
@@ -28,11 +29,11 @@ function App() {
       dashboard={dashboard}
       dataProvider={jsonServerProvider('https://jsonplaceholder.typicode.com')}
       >
-        <Resource name="dashboard"/>
+        <Resource name="dashboard" icon={HomeWorkIcon} />
         <Resource
         name='posts'
         list={PostList}
-        icon={HomeWorkIcon}
+        icon={MenuBookIcon}
         create={PostCreate}
         edit={PostEdit}
       />
