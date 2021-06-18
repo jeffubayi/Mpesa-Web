ccedc1311ef3860bd3769f6f60282e9d3bed4ae0
jeffubayi <jeffubayi@gmail.com>
2021-06-18 22:35:04 +0300

    add login styles

---
+++ client/src/components/Auth/styles.js
@@ -0,0 +1,31 @@
+
+const styles = {
+    root: {
+        height: '100vh',
+      },
+      image: {
+        backgroundImage: 'url( https://static02.astro.com.my/astro/media/astromain/packagepromotion/astro_share_1.png)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:"#FFFFF",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      },
+      paper: {
+        marginTop:" 5rem " ,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        height:"5rem",
+        width:"5rem"
+      },
+      form: {
+        width: '70%',
+        marginTop:" 5rem " ,
+      },
+      submit: {
+        margin: "2",
+      },
+  };
+  export default styles;
