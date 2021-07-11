b3654a843bcbb614ca24ccf8d6c7b719281da635
jeffubayi <jeffubayi@gmail.com>
2021-07-11 20:31:52 +0300

    update dashboard App

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,30 +1,25 @@
 // material
-import { Box, Grid, Container, Typography } from '@material-ui/core';
+import { Box, Grid, Container, Typography } from "@material-ui/core";
 // components
-import Page from '../components/Page';
+import Page from "../components/Page";
 import {
-  AppTasks,
+  AppCurrentVisits,
   AppNewUsers,
   AppBugReports,
   AppItemOrders,
   AppNewsUpdate,
   AppWeeklySales,
-  AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
-  AppCurrentSubject,
-  AppConversionRates
-} from '../components/_dashboard/app';
+} from "../components/_dashboard/app";
 
 // ----------------------------------------------------------------------
 
 export default function DashboardApp() {
   return (
-    <Page title="Dashboard | Minimal-UI">
+    <Page title="Home | M-PESA">
       <Container maxWidth="xl">
         <Box sx={{ pb: 5 }}>
-          <Typography variant="h4">Hi, Welcome back</Typography>
+          <Typography variant="h5">Current Balance</Typography>
+          <Typography variant="h4"> Ksh. 100</Typography>
         </Box>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
@@ -41,13 +36,13 @@ export default function DashboardApp() {
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
-            <AppWebsiteVisits />
+            <AppNewsUpdate />
           </Grid>
 
           <Grid item xs={12} md={6} lg={4}>
             <AppCurrentVisits />
           </Grid>
-
+{/* 
           <Grid item xs={12} md={6} lg={8}>
             <AppConversionRates />
           </Grid>
@@ -70,7 +65,7 @@ export default function DashboardApp() {
 
           <Grid item xs={12} md={6} lg={8}>
             <AppTasks />
-          </Grid>
+          </Grid> */}
         </Grid>
       </Container>
     </Page>
