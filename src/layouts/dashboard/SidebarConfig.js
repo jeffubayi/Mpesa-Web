5cece3a579d5d73ba824de424af30ba567b56505
jeffubayi <jeffubayi@gmail.com>
2021-08-07 18:12:08 +0300

    quality - adjust the sidebar url configs/routes

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -1,12 +1,11 @@
 import { Icon } from '@iconify/react';
 import pieChart2Fill from '@iconify/icons-eva/pie-chart-2-fill';
-import peopleFill from '@iconify/icons-eva/people-fill';
-import shoppingBagFill from '@iconify/icons-eva/shopping-bag-fill';
-import fileTextFill from '@iconify/icons-eva/file-text-fill';
-import lockFill from '@iconify/icons-eva/lock-fill';
-import personAddFill from '@iconify/icons-eva/person-add-fill';
-import alertTriangleFill from '@iconify/icons-eva/alert-triangle-fill';
-
+import HomeIcon from '@material-ui/icons/Home';
+import AddToHomeScreenIcon from '@material-ui/icons/AddToHomeScreen';
+import ExploreIcon from '@material-ui/icons/Explore';
+import SettingsIcon from '@material-ui/icons/Settings';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 // ----------------------------------------------------------------------
 
 const getIcon = (name) => <Icon icon={name} width={22} height={22} />;
@@ -15,37 +14,37 @@ const sidebarConfig = [
   {
     title: 'home',
     path: '/dashboard/app',
-    icon: getIcon(pieChart2Fill)
+    icon: <HomeIcon/>,
   },
   {
     title: 'transact',
     path: '/dashboard/user',
-    icon: getIcon(peopleFill)
+    icon: <AddToHomeScreenIcon/>,
   },
   {
     title: 'discover',
     path: '/dashboard/products',
-    icon: getIcon(shoppingBagFill)
+    icon: <ExploreIcon/>,
   },
   {
     title: 'my spend',
     path: '/dashboard/blog',
-    icon: getIcon(fileTextFill)
+    icon: getIcon(pieChart2Fill)
   },
   {
     title: 'grow',
-    path: '/login',
-    icon: getIcon(lockFill)
+    path: '/404',
+    icon: <TrendingUpIcon/>,
   },
   {
     title: 'settings',
     path: '/register',
-    icon: getIcon(personAddFill)
+    icon: < SettingsIcon/>,
   },
   {
     title: 'logout',
-    path: '/404',
-    icon: getIcon(alertTriangleFill)
+    path: '/login',
+    icon: <ExitToAppIcon/>,
   }
 ];
 
