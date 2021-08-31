0c94efb6bb0b15b8f75f322a01ff35885c32262c
jeffubayi <jeffubayi@gmail.com>
2021-08-31 15:15:45 +0300

    users - update the users data

--- src/_mocks_/user.js
+++ src/_mocks_/user.js
@@ -9,21 +9,10 @@ const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
   avatarUrl: mockImgAvatar(index + 1),
   name: faker.name.findName(),
-  company: faker.company.companyName(),
-  isVerified: faker.datatype.boolean(),
-  status: sample(['active', 'banned']),
-  role: sample([
-    'Leader',
-    'Hr Manager',
-    'UI Designer',
-    'UX Designer',
-    'UI/UX Designer',
-    'Project Manager',
-    'Backend Developer',
-    'Full Stack Designer',
-    'Front End Developer',
-    'Full Stack Developer'
-  ])
+  company:faker.phone.phoneNumberFormat(),
+  date: faker.date.past(),
+  status: sample(['sent', 'declined']),
+  role: faker.commerce.price(),
 }));
 
 export default users;
