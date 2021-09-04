bb0085162769c3813960ef86586f88eab7326642
jeffubayi <jeffubayi@gmail.com>
2021-09-04 17:07:13 +0300

    transact - add mpesa send money CTA with button

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,22 +1,42 @@
-
 // material
-import { Container  } from '@material-ui/core';
+import { Container } from "@material-ui/core";
 // components
-import Page from '../components/Page';
-import {
-  ProductCartWidget,
-} from '../components/_dashboard/products';
-import Checkout from "../components/MpesaCheckout/Checkout"
+import Page from "../components/Page";
+import { ProductCartWidget } from "../components/_dashboard/products";
+import Checkout from "../components/MpesaCheckout/SendMoneyDialog";
 // ----------------------------------------------------------------------
+import Button from "@material-ui/core/Button";
+import React from "react";
 
-export default function EcommerceShop({products,...rest}) {
+export default function EcommerceShop({ products, ...rest }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Page title="Transact | Mpesa">
       <Container>
-       
-       < Checkout/>
-        <ProductCartWidget /> 
+        <div>
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+            Send money
+          </Button>
+          {open && (
+            <Checkout
+              openDialog={handleClickOpen}
+              closeDialog={handleClose}
+              buttonHelperText="Close"
+              title="Warning"
+              description="You lack permissions to view the requested resources"
+            />
+          )}
+        </div>
+        <ProductCartWidget />
       </Container>
     </Page>
   );
