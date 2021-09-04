dbf726de7ac250d193acda069d2401d850a9acfc
jeffubayi <jeffubayi@gmail.com>
2021-09-04 17:49:21 +0300

    dashboard -add an onclick to launch the transaction dialog

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -42,17 +42,17 @@ export default function DashboardApp() {
           <Typography variant="h4"> Ksh. 100</Typography>
         </Box>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3} onClick={handleClickOpen}>
             <AppWeeklySales />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3} onClick={handleClickOpen}>
           <AppItemOrders />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3} onClick={handleClickOpen}>
             
             <AppBugReports />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3} onClick={handleClickOpen}>
             
             <AppNewUsers />
           </Grid>
