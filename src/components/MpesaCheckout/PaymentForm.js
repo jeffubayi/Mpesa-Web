069aa313ff9434f8690179cafaeda10b8aaba72e
jeffubayi <jeffubayi@gmail.com>
2021-09-08 11:58:31 +0300

    transact - update the transaction details step

--- src/components/MpesaCheckout/PaymentForm.js
+++ src/components/MpesaCheckout/PaymentForm.js
@@ -4,21 +4,29 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import SearchContact from './SearchContact';
+
 
 export default function PaymentForm() {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Payment method
+        Add transaction amount
       </Typography>
-      <SearchContact/>
       <Grid container spacing={3}>
+      <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cardNumber"
+            label="Enter Amount"
+            fullWidth
+            autoComplete="cc-number"
+          />
+        </Grid>
         <Grid item xs={12} md={6}>
           <TextField
             required
             id="cardNumber"
-            label="Enter Phone number"
+            label="Enter Mpesa Pin"
             fullWidth
             autoComplete="cc-number"
           />
