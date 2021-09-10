bf81da6b81bf4a3540cbc22423e2d417a7bc29f6
jeffubayi <jeffubayi@gmail.com>
2021-09-10 21:27:44 +0300

    update Review  form page

--- src/components/MpesaCheckout/Review.js
+++ src/components/MpesaCheckout/Review.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import {Typography,Avatar} from '@material-ui/core';
+import {Typography,Avatar,Divider} from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const payments = [
   { name: 'Amount Ksh', detail: '5,000' },
@@ -28,21 +30,19 @@ export default function Review() {
 
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Confirm transaction
-      </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <Typography variant="h6" gutterBottom className={classes.title}>
-            Sent to 
+            Sent to :
           </Typography>
           <Avatar style={{width:"3.8rem",height:"3.8rem"}}/>
           <Typography gutterBottom>John Smith</Typography>
           <Typography gutterBottom>07070707070</Typography>
         </Grid>
+        <Divider/>
         <Grid item container direction="column" xs={12} sm={6}>
           <Typography variant="h6" gutterBottom className={classes.title}>
-            transaction details
+            Transaction details:
           </Typography>
           <Grid container>
             {payments.map((payment) => (
@@ -56,6 +56,12 @@ export default function Review() {
               </React.Fragment>
             ))}
           </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="primary" name="saveCard" value="yes" />}
+            label="Send a payment receipt"
+          />
         </Grid>
       </Grid>
     </React.Fragment>
