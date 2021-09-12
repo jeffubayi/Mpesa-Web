0f3d8a6109f1d66e0746059abff8228ea5506720
jeffubayi <jeffubayi@gmail.com>
2021-09-12 12:50:32 +0300

    update the myspend/transaction components

--- src/components/_dashboard/app/AppWebsiteVisits.js
+++ src/components/_dashboard/app/AppWebsiteVisits.js
@@ -1,68 +1,75 @@
-import { merge } from 'lodash';
-import ReactApexChart from 'react-apexcharts';
+import { merge } from "lodash";
+import ReactApexChart from "react-apexcharts";
 // material
-import { Card, CardHeader, Box } from '@material-ui/core';
+import { Card, CardHeader, Box } from "@material-ui/core";
 //
-import { BaseOptionChart } from '../../charts';
+import { BaseOptionChart } from "../../charts";
+import { fDateMonth } from "../../../utils/formatTime";
 
 // ----------------------------------------------------------------------
 
 const CHART_DATA = [
   {
-    name: 'Team A',
-    type: 'column',
-    data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]
+    name: "FAMILY AND FRIENDS",
+    type: "column",
+    data: [23, 11, 22, 27, 13, 22, 37, 21],
   },
   {
-    name: 'Team B',
-    type: 'area',
-    data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
+    name: "GENERAL",
+    type: "area",
+    data: [44, 55, 41, 67, 22, 43, 21, 41],
   },
   {
-    name: 'Team C',
-    type: 'line',
-    data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
-  }
+    name: "BILLS AND SERVICES",
+    type: "line",
+    data: [30, 25, 36, 30, 45, 35, 64, 52],
+  },
+  {
+    name: "WITHDRAWALS",
+    type: "column",
+    data: [20, 5, 4, 27, 22, 23, 61, 11],
+  },
 ];
 
 export default function AppWebsiteVisits() {
   const chartOptions = merge(BaseOptionChart(), {
     stroke: { width: [0, 2, 3] },
-    plotOptions: { bar: { columnWidth: '11%', borderRadius: 4 } },
-    fill: { type: ['solid', 'gradient', 'solid'] },
+    plotOptions: { bar: { columnWidth: "11%", borderRadius: 4 } },
+    fill: { type: ["solid", "gradient", "solid"] },
     labels: [
-      '01/01/2003',
-      '02/01/2003',
-      '03/01/2003',
-      '04/01/2003',
-      '05/01/2003',
-      '06/01/2003',
-      '07/01/2003',
-      '08/01/2003',
-      '09/01/2003',
-      '10/01/2003',
-      '11/01/2003'
+      "0",
+      "1",
+      "5",
+      "10",
+      "15",
+      "20",
+      "25",
+      "30",
     ],
-    xaxis: { type: 'datetime' },
     tooltip: {
       shared: true,
       intersect: false,
       y: {
         formatter: (y) => {
-          if (typeof y !== 'undefined') {
-            return `${y.toFixed(0)} visits`;
+          if (typeof y !== "undefined") {
+            return `Ksh :${y.toFixed(0)} `;
           }
           return y;
-        }
-      }
-    }
+        },
+      },
+    },
   });
 
   return (
     <Card>
-      <CardHeader title="M-pesa Statement" subheader="(+43%) than last year" />
+      <CardHeader title={fDateMonth(new Date())} subheader="KSH : 4,000" />
       <Box sx={{ p: 3, pb: 1 }} dir="ltr">
-        <ReactApexChart type="line" series={CHART_DATA} options={chartOptions} height={364} />
+        <ReactApexChart
+          type="line"
+          series={CHART_DATA}
+          options={chartOptions}
+          height={364}
+        />
       </Box>
     </Card>
   );
