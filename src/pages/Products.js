8c8b04ad186f8a5708a9c47051c5cd7f6a0bfbb4
jeffubayi <jeffubayi@gmail.com>
2021-09-29 18:58:15 +0300

    styles -make transactions page responsive

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -9,7 +9,6 @@ import { makeStyles } from "@material-ui/styles";
 import React from "react";
 import TransactCard from "./TransactCard";
 import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,16 +18,17 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1),
     textAlign: "center",
     color: "#000",
     fontSize: "0.8rem",
     fontWeight: 600,
     backgroundColor: "#F4F6F8",
+    margin: "1rem",
   },
 }));
 
-export default function EcommerceShop({ products, ...rest }) {
+export default function Transaction({ products, ...rest }) {
   const [open, setOpen] = React.useState(false);
   const classes = useStyles();
 
@@ -57,102 +57,103 @@ export default function EcommerceShop({ products, ...rest }) {
             Transact
           </Typography>
           <div>
-            <Grid container spacing={5}>
-              <Grid item xs={6} spacing={3}>
-                <Paper className={classes.paper}>SEND AND REQUEST</Paper>
-              </Grid>
-              <Grid item xs={6}>
-                <Paper className={classes.paper}>PAYMENT</Paper>
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  send
-                  title="SEND MONEY"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  send
-                  title="REQUEST MONEY"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  pay
-                  title="PAY BILL"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  pay
-                  title="BUY GOODS"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  send
-                  title="GLOBAL"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen} >
-                <TransactCard
-                  send
-                  title="REQUEST ANOTHER"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  pay
-                  title="POCHI LA BIASHARA"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  pay
-                  title="FULIZA"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <Paper className={classes.paper}>WITHDRAW</Paper>
-              </Grid>
-              <Grid item xs={6}>
-                <Paper className={classes.paper}>BUY AIRTIME</Paper>
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen}>
-                <TransactCard
-                  withdraw
-                  title="AT AGENT"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen} >
-                <TransactCard
-                  withdraw
-                  title="AT ATM"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen} >
-                <TransactCard
-                  title="FOR MY NUMBER"
-                  className={classes.transactCard}
-                />
-              </Grid>
-              <Grid item xs={3} onClick={handleClickOpen} >
-                <TransactCard
-                  title="FOR ANOTHER NUMBER"
-                  className={classes.transactCard}
-                />
-              </Grid>
-            </Grid>
+            <Paper className={classes.paper}>SEND</Paper>
+            <div
+              style={{
+                display: "grid",
+                gap: "1.2rem",
+                gridTemplateColumns: "repeat(2,auto)",
+              }}
+            >
+              <TransactCard
+                send
+                title="SEND MONEY"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                send
+                title="REQUEST MONEY"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                send
+                title="SEND MONEY"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                send
+                title="REQUEST MONEY"
+                className={classes.transactCard}
+              />
+            </div>
+
+            <Paper className={classes.paper}>PAYMENT</Paper>
+            <div
+              style={{
+                display: "grid",
+                gap: "1.2rem",
+                gridTemplateColumns: "repeat(2,auto)",
+              }}
+            >
+              <TransactCard
+                pay
+                title="PAY BILL"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                pay
+                title="REQUEST MONEY"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                pay
+                title="SEND MONEY"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                pay
+                title="FULIZA"
+                className={classes.transactCard}
+              />
+            </div>
+
+            <Paper className={classes.paper}>WITHDRAW</Paper>
+            <div
+              style={{
+                display: "grid",
+                gap: "1.2rem",
+                gridTemplateColumns: "repeat(2,auto)",
+              }}
+            >
+              <TransactCard
+                withdraw
+                title="AT ATM"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                withdraw
+                title="AT AGENT"
+                className={classes.transactCard}
+              />
+            </div>
+
+            <Paper className={classes.paper}>BUY AIRTIME</Paper>
+            <div
+              style={{
+                display: "grid",
+                gap: "1.2rem",
+                gridTemplateColumns: "repeat(2,auto)",
+              }}
+            >
+              <TransactCard
+                title="FOR MY NUMBER"
+                className={classes.transactCard}
+              />
+              <TransactCard
+                title="FOR ANOTHER NUMBER"
+                className={classes.transactCard}
+              />
+            </div>
           </div>
           <ProductCartWidget />
         </Container>
