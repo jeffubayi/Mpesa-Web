9c55904a2cddd9c1da38e0e928c7c8c2c36af35c
jeffubayi <jeffubayi@gmail.com>
2021-10-09 08:02:27 +0300

    styles - make my spend page responsive

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,8 +1,5 @@
-import { Icon } from "@iconify/react";
-import plusFill from "@iconify/icons-eva/plus-fill";
-import { Link as RouterLink } from "react-router-dom";
 // material
-import { Stack, Button, Container, Typography } from "@material-ui/core";
+import { Stack, Container, Typography } from "@material-ui/core";
 // components
 import Page from "../components/Page";
 import ExpenseChart from "../components/_dashboard/app/AppWebsiteVisits";
@@ -24,14 +21,6 @@ export default function User() {
           <Typography variant="h4" gutterBottom>
             My Spend
           </Typography>
-          <Button
-            variant="contained"
-            component={RouterLink}
-            to="#"
-            startIcon={<Icon icon={plusFill} />}
-          >
-            LOANS AND SAVINGS
-          </Button>
         </Stack>
         <MySpend/>
         <ExpenseChart />
